eb6924eac315e927c05bc145f0ebe824454422c0
Matheus Amâncio <matheus.amancio01@gmail.com>
2022-11-10 17:46:28 -0300

    [Base dos arquivos header.php e footer.php criados + Include ok para organização da aplicação]

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Desafio Estoque Now</title>
+        <!-- CSS -->
+        <link rel="stylesheet" href="./styles.css">
+        <!-- /CSS -->
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- /Bootstrap -->
+    </head>
+    <body>
+        <!-- NAVBAR -->
+        <header>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid d-flex justify-content-space">
+                    <a class="navbar-brand" href="#">Estoque Now</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                            <a class="nav-link" href="add-product.php">Adicionar Produtos</a>
+                            <a class="nav-link" href="product-list.php">Produtos</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <!-- /NAVBAR -->
